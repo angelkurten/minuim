005c7d82556907cd8b448620ca92b010e526d9ab
AngelKurten <angel@orandev.tech>
2020-10-21 16:54:36 -0500

    fic connection

--- apps/mu-platform/src/app.module.ts
+++ apps/mu-platform/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module, Global } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MuLogger } from '../../../libs/common/src/logging';
 import { NotificationModule } from './notification/notification.module';
@@ -9,9 +9,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, }),
-    TypeOrmModule.forRoot({
-      autoLoadEntities:true,
-      synchronize:false
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     NotificationModule,
   ],
